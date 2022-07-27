d06730810be3ffda392f0c21e0521b7cb8f18cbd
Armando Rosales <refill.1509@gmail.com>
2022-07-26 21:09:19 -0600

    se agrego la funcion imprimir y validar_x(sin terminar)

--- Gato/gato.py
+++ Gato/gato.py
@@ -1,16 +1,37 @@
+from turtle import position
+
+
 def reinicio():
-    estructura = [['o','│','x','│','o'],
-                  ['x','│','x','│','x'],
-                  ['x','│','x','│','x']]
+    estructura = [['x','x','x'],
+                  ['x','x','x'],
+                  ['x','x','x']]
     return estructura
+
+
+def imprimir(estructura):
+    for i in range(3):
+        for j in range(3):
+            print(estructura[i][j] + ' ', end='')
+        print()
+
+
+def validar_x(estructura, gano_x):
+    posiciones =[[0, 0, 0], #Horizontal position
+                 [0, 0, 0], #Vertical position
+                 [0, 0, 0]] #Diagonal position
+    for i in range(3):
+        for j in range(3):
+            if estructura[i][j] == 'x':
+                posiciones[i][j] = posiciones[i][j] + 1
+    
+    print(posiciones)
+    return gano_x
 
 def run():
     estructura = []
+    x = bool
     estructura = reinicio()
-    for i in range(3):
-        for j in range(5):
-            print(estructura[i][j], end='')
-        print()
+    validar_x(estructura, x)
 
 if __name__ == '__main__':
     run()
